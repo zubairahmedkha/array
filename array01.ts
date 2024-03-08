6ade637faa7a8cbf62d8225e4d9667ddbb1f4f06
zubairkhan <zubairahmedniz@gmail.com>
2024-03-08 09:09:08 +0500

    arraywork

--- array01.ts
+++ array01.ts
@@ -1,3 +1,18 @@
+//let pets =["tiger","dog","cow","horse"];
+//pets.pop();
+//console.log(pets)
 let pets =["tiger","dog","cow","horse"];
-pets.pop();
+pets.push("dogyy","lion",);
 console.log(pets)
+let pet1 =["tiger","dog","cow","horse"];
+pet1.shift();
+console.log(pet1)
+let pet2 =["tiger","dog","cow","horse"];
+pet2.unshift("dogyy","lion",);
+console.log(pet2)
+let pet3 =["tiger","dog","cow","horse"];
+pet3.splice(2,0,"lion","loom");
+console.log(pet3);
+
+
+
